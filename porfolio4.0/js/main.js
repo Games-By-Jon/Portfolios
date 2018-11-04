6e94eef01e861ba8927065152aee672309a38d4b
Jon <jonathan@iMac.local>
2018-11-03 21:19:21 -0400

    4.0 upload

---
+++ porfolio4.0/js/main.js
@@ -0,0 +1,102 @@
+/* =================================
+------------------------------------
+	Game Warrior Template
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
+
+
+'use strict';
+
+
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+(function($) {
+
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.nav-switch').on('click', function(event) {
+		$('.main-menu').slideToggle(400);
+		event.preventDefault();
+	});
+
+
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	/*------------------
+		Hero Slider
+	--------------------*/
+	$('.hero-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		mouseDrag: false,
+		animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+		items: 1,
+		autoplay: true
+	});
+	var dot = $('.hero-slider .owl-dot');
+	dot.each(function() {
+		var index = $(this).index() + 1;
+		if(index < 10){
+			$(this).html('0').append(index);
+			$(this).append('<span>.</span>');
+		}else{
+			$(this).html(index);
+			$(this).append('<span>.</span>');
+		}
+	});
+
+
+	/*------------------
+		News Ticker
+	--------------------*/
+	$('.news-ticker').marquee({
+	    duration: 10000,
+	    //gap in pixels between the tickers
+	    //gap: 200,
+	    delayBeforeStart: 0,
+	    direction: 'left',
+	    duplicated: true
+	});
+
+	/*------------------
+		News Ticker
+	--------------------*/
+	$(document).ready(function(){ 
+    $('#characterLeft').text('280 characters left');
+    $('#message').keydown(function () {
+        var max = 140;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });    
+});
+
+})(jQuery);
+
